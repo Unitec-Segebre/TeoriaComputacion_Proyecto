6727a6fe832fb1df3aa860c26e86e8cc5140e48d
Juan Enrique Segebre <juansegebre@gmail.com>
2017-08-07 01:32:08 -0600

    Closure correctly works on one state

--- AutomataSolver.py
+++ AutomataSolver.py
@@ -59,36 +59,72 @@ class Automata_NFA(Automata_BARE):
 class Automata_NFAEpsilon(Automata_BARE):
     def __init__(self, states, input_symbols, transitions, initial_state, final_states):
         super(Automata_NFAEpsilon, self).__init__(states, input_symbols, transitions, initial_state, final_states)
+
+
+
+    def solve(self, sequence, epsilon):
         print("---HERE---")
         print(self.states)
         print(self.input_symbols)
         print(self.transitions)
         print(self.initial_states)
         print(self.final_states)
+        for state in self.transitions:
+            free_states = set()
+            print("{}".format(self.closure(epsilon, state, free_states)))
 
-    def solve(self, sequence, epsilon):
-        current_states = [self.initial_states]
-        for symbol in sequence:
-            current_states_temp = []
-            for current_state in current_states:
-                if current_state in self.transitions:
-                    for arista in self.transitions[current_state]:
-                        if symbol == arista:
-                            for destiny in self.transitions[current_state][arista]:
-                                current_states_temp.append(destiny)
-                else:
-                    raise Exception('{} is NOT a valid state'.format(current_state))
-            current_states = list(set(current_states_temp))
-            if current_states == []:
-                break
-        for current_state in current_states:
-            if current_state in self.final_states:
-                return current_state
-        raise Exception('{} is NOT a solution'.format(sequence))
-
-    def clousure(self, epsilon, state, list):
+    def closure(self, epsilon, state, free_states):
+        free_states.add(state)
         if epsilon in self.transitions[state]:
-            for closure_state in self.transitions[state][epsilon]:
-                self.clousure(epsilon, state, list)
-        list.append(state)
-        return
+            for destiny in self.transitions[state][epsilon]:
+                if destiny not in free_states:
+                    self.closure(epsilon, destiny, free_states)
+        return free_states
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def solve(self, sequence, epsilon):
+    #     current_states = [self.initial_states]
+    #     for symbol in sequence:
+    #         for state in current_states:
+    #             states_to_add = []
+    #             self.clousure(epsilon, state, states_to_add)
+    #             for state_to_add in states_to_add:
+    #                 current_states.append(state_to_add)
+    #         current_states = list(set(current_states))
+    #         current_states_temp = []
+    #         for current_state in current_states:
+    #             if current_state in self.transitions:
+    #                 for arista in self.transitions[current_state]:
+    #                     if symbol == arista:
+    #                         for destiny in self.transitions[current_state][arista]:
+    #                             current_states_temp.append(destiny)
+    #             else:
+    #                 raise Exception('{} is NOT a valid state'.format(current_state))
+    #         current_states = list(set(current_states_temp))
+    #         if current_states == []:
+    #             break
+    #     for current_state in current_states:
+    #         if current_state in self.final_states:
+    #             return current_state
+    #     raise Exception('{} is NOT a solution'.format(sequence))
+    #
+    # def clousure(self, epsilon, state, list):
+    #     if epsilon in self.transitions[state]:
+    #         for closure_state in self.transitions[state][epsilon]:
+    #             list.append(state)
+    #             print(closure_state)
+    #             if closure_state in list:
+    #                 continue
+    #             self.clousure(epsilon, closure_state, list)
+    #     return
