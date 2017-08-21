5592f7bce3a8a7a5172ca036af35acee53b36135
Juan Enrique Segebre <juansegebre@gmail.com>
2017-08-21 13:41:54 -0600

    Added no input message

--- RegEx_graph.py
+++ RegEx_graph.py
@@ -4,6 +4,8 @@ class RegEx_graph(QMessageBox):
     def __init__(self, parent=None, expresion=None):
         super(RegEx_graph, self).__init__(parent)
         self.setWindowTitle("Regular Expression")
+        if expresion == "()*":
+            expresion = "The only solution is no input!"
         self.setText(expresion)
         self.setStandardButtons(QMessageBox.Ok)
         self.show()
