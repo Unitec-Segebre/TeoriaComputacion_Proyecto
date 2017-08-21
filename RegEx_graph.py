4a19d83592c6b530b3cc8717917531ca1be6d68f
Juan Enrique Segebre <juansegebre@gmail.com>
2017-08-21 14:24:49 -0600

    Changed regex to QInputBox

--- RegEx_graph.py
+++ RegEx_graph.py
@@ -1,11 +1,12 @@
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QInputDialog, QLineEdit
 
-class RegEx_graph(QMessageBox):
+class RegEx_graph(QInputDialog):
     def __init__(self, parent=None, expresion=None):
         super(RegEx_graph, self).__init__(parent)
         self.setWindowTitle("Regular Expression")
         if expresion == "()*":
             expresion = "The only solution is no input!"
-        self.setText(expresion)
-        self.setStandardButtons(QMessageBox.Ok)
-        self.show()
+        self.getText(parent, "Regular Expression to Epsilon NFA", "Expression:", QLineEdit.Normal, expresion)
+        # self.setText(expresion)
+        # self.setStandardButtons(QMessageBox.Ok)
+        # self.show()
